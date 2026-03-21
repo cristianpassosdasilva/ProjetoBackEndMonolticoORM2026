b24b2a39921346ec22c50e46ff2d66fb80737798
teste <you@example.com>
2026-03-21 09:47:30 -0300

    Atualizando o projeto Contatos

--- contatos/src/main/java/br/edu/iftm/pbackorm/contatos/controller/ContatoController.java
+++ contatos/src/main/java/br/edu/iftm/pbackorm/contatos/controller/ContatoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,10 +65,33 @@ public class ContatoController {
             .body(new ErroDTO("Já existe contato de código "+novoContato.getCodigo()
                             ,LocalDateTime.now()));
         }
+        if (novoContato.getNome() == null || novoContato.getNome().isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErroDTO("Contato com nome vazio",LocalDateTime.now()));
+        }
         contatos.add(novoContato);
         return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body(novoContato);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable Integer id,
+                                       @RequestBody Contato contatoAtualizado) {
+        for (Contato contato: contatos) {
+            if (contato.getCodigo().equals(id)) {
+                if (contatoAtualizado.getNome() == null || 
+                    contatoAtualizado.getNome().isBlank()) {
+                     return ResponseEntity.status(HttpStatus.BAD_REQUEST) 
+                        .body(new ErroDTO("Contato com nome vazio",LocalDateTime.now()));
+                }
+                contato.setNome(contatoAtualizado.getNome());
+                return ResponseEntity.ok(contatoAtualizado);
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            new ErroDTO("Contato não encontrado "+id,LocalDateTime.now()));
     }
 
     @DeleteMapping("/{id}")
@@ -77,7 +101,8 @@ public class ContatoController {
         if (removido) {
             return ResponseEntity.noContent().build();
         }    
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            new ErroDTO("Contato não encontrado "+id, LocalDateTime.now()));
     }
 
 }
